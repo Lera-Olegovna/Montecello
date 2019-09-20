497cfeacd02b036dd604953c06e97697f4da96ce
Valeriya <leraolegovna1986@gmail.com>
2019-09-20 21:32:36 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,214 @@
+$(document).ready(function(){
+	'use strict'
+	$('.slider-header').slick({
+		dots		:true,
+		dotsClass:'slider-header__dots',
+		prevArrow: false,
+		nextArrow: false,
+		slidesToShow: 1,
+  		slidesToScroll: 1
+
+	});
+	$('.slider-news').slick({
+		dots		:true,
+		dotsClass:'slider-news__dots',
+		prevArrow:'<button class="slider-news__arrow slider-news__arrow--prev">></button>',
+		nextArrow:'<button class="slider-news__arrow slider-news__arrow--next">></button>',
+		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		autoplay: true,
+  		autoplaySpeed: 4000
+
+	});
+});
+
+function initMap() {
+	let address = {
+	       
+	    lat: 40.689457,
+	    lng: -73.934388
+	}
+
+	let centerMap = {
+	         
+	    lat: 40.675276,
+	    lng: -73.904117
+	}
+	    
+	let map = new google.maps.Map(
+	    document.getElementById('map'), {
+	    zoom: 12,
+	    center: centerMap,
+	    styles: [
+  {
+    "elementType": "geometry",
+   "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+]
+
+
+
+    });
+
+    let marker = new google.maps.Marker({
+        position: address,
+        map: map,
+        icon: './img/marker.svg',
+        title: '91 Nolan Extensions Suite 670'
+    });
+};
+
